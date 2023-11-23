9a820a0a236ccf20117012b8aee485b5b1ce0e7c
Tomas Bortoli <tomasbortoli@tutanota.com>
2023-11-23 21:57:43 +0100

    Remove CSS style attribute from `dicono-di-me.html` because it did not improve scrolling on iOS.
    Fixed mobile version of `dicono-di-me.html` as the previous commit obviously broke it.
    Figured out why there was the JS WTF in `mobile_about_us.js`. When setting `className = ''`, apparently the array of extracted elements with `getElementsByClassName was getting auto updated; so it was shrinking one element when removing the className attribute from one of its elements (!!)

--- scripts/mobile_about_us.js
+++ scripts/mobile_about_us.js
@@ -1,18 +1,3 @@
-// The JS gods don't want the below function to exist so copy-paste below :'(
-/*function set_mobile_style(){
-  elements = document.getElementsByClassName('dice-di-me')
-  for(i=0;i<elements.length;i++){
-    elements[i].className = ''
-    elements[i].width = '90%' //window.screen.width+'px'
-    elements[i].marginLeft = '0px'
-
-    elements[i].style.float = ''
-    elements[i].style.marginTop = ''
-    elements[i].style.marginRight = ''
-
-  }
-}*/
-
 function setup_mobile(){
   fix_common()
 
@@ -25,58 +10,20 @@ function setup_mobile(){
   getById('immagine-sfondo').style.backgroundSize = 'auto 100%'
   getById('spazi-sotto-immagine-sfondo').innerHTML = '<br><br><br>'
 
-  // JS ultimate WTF
-  //calling a function 4 times does NOT work. DON'T ASK ME WHY!!
-  //for(i-0;i<4;i++){
-  //  set_mobile_style()
-  //}
-
-  try{  //need the try catch cause this will error and stopping the execution would prevent the rest of the setup from working properly
-    elements = document.getElementsByClassName('dice-di-me')
-    for(i=0;i<elements.length;i++){
-      elements[i].className = ''
-      elements[i].width = '90%' //window.screen.width+'px'
-      elements[i].marginLeft = '0px'
-
-      elements[i].style.float = ''
-      elements[i].style.marginTop = ''
-      elements[i].style.marginRight = ''
-
-    }
-    elements = document.getElementsByClassName('dice-di-me')
-    for(i=0;i<elements.length;i++){
-      elements[i].className = ''
-      elements[i].width = '90%' //window.screen.width+'px'
-      elements[i].marginLeft = '0px'
-
-      elements[i].style.float = ''
-      elements[i].style.marginTop = ''
-      elements[i].style.marginRight = ''
-
-    }
-    elements = document.getElementsByClassName('dice-di-me')
-    for(i=0;i<elements.length;i++){
-      elements[i].className = ''
-      elements[i].width = '90%' //window.screen.width+'px'
-      elements[i].marginLeft = '0px'
-
-      elements[i].style.float = ''
-      elements[i].style.marginTop = ''
-      elements[i].style.marginRight = ''
-
-    }
-    elements = document.getElementsByClassName('dice-di-me')
-    for(i=0;i<elements.length;i++){
-      elements[i].className = ''
-      elements[i].width = '90%' //window.screen.width+'px'
-      elements[i].marginLeft = '0px'
-
-      elements[i].style.float = ''
-      elements[i].style.marginTop = ''
-      elements[i].style.marginRight = ''
-
-    }
+  lines = document.getElementsByClassName('line-dicono')
+  for(i=0;i<lines.length;i+=0){
+        lines[i].className = ''
   }
-  catch(e){}
 
+    elements = document.getElementsByClassName('dice-di-me')
+    for(i=0;i<elements.length;i+=0){
+      elements[i].className = ''
+      elements[i].width = '90%' //window.screen.width+'px'
+      elements[i].style.marginLeft = '0px'
+
+      elements[i].style.float = ''
+      elements[i].style.marginTop = ''
+      elements[i].style.marginRight = ''
+
+    }
 }
